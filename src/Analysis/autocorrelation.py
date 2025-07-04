Optimised Analysis module, fixed bug with multiple data series in plot, added Open file for multiple files, added in choose samples selection of a file - calculation of all the sheets

src/Analysis/autocorrelation.py
@@ -1,220 +1,61 @@
-from ui.configuration.configuration_autocorrelation_window import Ui_ConfigurationAutocorrelationWindow
-from ui.configuration.choose_sample_window import Ui_ChooseSampleWindow
-from ui.plot.plot_window import Ui_Plot
-from src.Plot.plot import PlotDialog
-from src.data_model import DataModel
-import pandas as pd
 import numpy as np
 import re
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
 
-from PyQt6.QtWidgets import (
-    QWidget, QDialog, QMessageBox, QTreeWidgetItem, QTabWidget, QTableWidget, QTableWidgetItem, QVBoxLayout
-)
 
-class UIAutocorrelation(QWidget):
-    def __init__(self, ui):
+class Autocorrelation():
+    def __init__(self, data, values):
         super().__init__()
-        self.ui = ui
-        self.ui.actionAutocorrelation.triggered.connect(self.open_dialog)
+        self.data = data
+        self.values = values
 
-    def open_dialog(self):
-        dialog = QDialog(self)
-        ui = Ui_ConfigurationAutocorrelationWindow()
-        ui.setupUi(dialog)
-        self.all_scalar_data = []
-        result = dialog.exec()
-        if result == QDialog.DialogCode.Accepted:
-            self.open_choose_samples_dialog()
-            self.values = self._read_values(ui)
-            if self.values is not None:
-                self.run_autocorrelation()
-        else:
-            return None
+        self.autocorrelation()
 
-    def _read_values(self, ui):
-        try:
-            time_interval = float(ui.lineEdit_time_interval.text())
-            n_time_points = int(ui.lineEdit_n_time_points.text())
-            n_tracks = int(ui.lineEdit_n_tracks.text())
-            n_plot_points = int(ui.lineEdit_n_plot_points.text())
-            return {
-                "time_interval": time_interval,
-                "n_time_points": n_time_points,
-                "n_tracks": n_tracks,
-                "n_plot_points": n_plot_points
-            }
-        except ValueError:
-            QMessageBox.warning(self, "Ошибка", "Пожалуйста, введите корректные числовые значения.")
-            return None
+    def autocorrelation(self):
+        # Normalised vector
+        self.norm()
+        # Average Scalars
+        self.scalars()
 
-    def open_choose_samples_dialog(self):
-        dialog = QDialog(self)
-        ui = Ui_ChooseSampleWindow()
-        ui.setupUi(dialog)
-        self.selected_samples = []
-
-        if self.ui.treeWidget:
-            ui.treeWidget.clear()
-            stack = []
-
-            # Клонируем верхний уровень
-            for i in range(self.ui.treeWidget.topLevelItemCount()):
-                src_item = self.ui.treeWidget.topLevelItem(i)
-                cloned_item = QTreeWidgetItem([src_item.text(0)])
-                ui.treeWidget.addTopLevelItem(cloned_item)
-                stack.append((src_item, cloned_item))
-
-            # Обход в ширину
-            while stack:
-                src_parent, cloned_parent = stack.pop(0)
-                for j in range(src_parent.childCount()):
-                    src_child = src_parent.child(j)
-                    cloned_child = QTreeWidgetItem([src_child.text(0)])
-                    cloned_parent.addChild(cloned_child)
-                    stack.append((src_child, cloned_child))
-            
-            
-            for i in range(ui.treeWidget.topLevelItemCount()):
-                ui.treeWidget.topLevelItem(i).setExpanded(True)
-
-            # Запускаем диалог и ждем результата
-            if dialog.exec() == QDialog.DialogCode.Accepted:
-                for item in ui.treeWidget.selectedItems():
-                    if item.parent() is not None:  # это лист (имеет родителя)
-                        file_path = item.parent().text(0)
-                        sheet_name = item.text(0)
-                        self.selected_samples.append([file_path, sheet_name])
-            else:
-                return None
-
-    def run_autocorrelation(self):
-        if self.values['time_interval'] == 0 or self.values['n_plot_points'] == 0:
-            QMessageBox.warning(self, "Ошибка", "Пожалуйста, введите корректные числовые значения.")
-
-        for filename in self.selected_samples:
-            # Find sheet
-            index = None
-            for i in range(self.ui.stackedWidget.count()):
-                widget = self.ui.stackedWidget.widget(i)
-                if getattr(widget, 'filename', None) == filename[0]:
-                    index = i
-            
-            if index is not None:
-                    self.ui.stackedWidget.setCurrentIndex(index)
-            tab_widget = self.ui.stackedWidget.currentWidget().findChild(QTabWidget)
-            if tab_widget:
-                for i in range(tab_widget.count()):
-                    if tab_widget.tabText(i) == filename[1]:
-                        tab_widget.setCurrentIndex(i)
-                        break
-            
-            table = tab_widget.widget(i)
-            data_model = table.model()
-            if not hasattr(data_model, "get_dataframe"):
-                continue
-
-            original_df = data_model.get_dataframe()
-            data = self.treat_data(original_df)
-            if data is None:
-                continue
-
-
-            # Normalised vector
-            norm_data = self.norm(data)
-            # Average Scalars
-            scalar_data = self.scalars(norm_data)
-            self.all_scalar_data.append((scalar_data, f"{filename[1]}"))
-
-            #Update table in tab:
-            # Обновляем модель
-            new_model = DataModel(scalar_data)
-            table.setModel(new_model)
-
-        # Create Statistics
-
-        # create plot
-        dialog = PlotDialog(self, title="MSD Plot")
-        dialog.show_plot(self.plot_scalar_averages)
-        #self.plot_scalar_averages()
-
-    def treat_data(self, df):
-        # Считать в pandas DataFrame
-        try:
-            df["Track n"] = df["Track n"].astype(int)
-            df["Slice n"] = df["Slice n"].astype(int)
-            df["X"] = pd.to_numeric(df["X"], errors='coerce')
-            df["Y"] = pd.to_numeric(df["Y"], errors='coerce')
-        except Exception as e:
-            QMessageBox.warning(self, "Ошибка", f"Не удалось преобразовать столбцы Track n или Slice n в числа:\n{e}")
-            return None
-
-        # Обработка треков
-        cleaned_rows = []
-        missing_tracks = []
-        for track_id in sorted(df["Track n"].unique()):
-            track_df = df[df["Track n"] == track_id].sort_values("Slice n")
-            if len(track_df) < self.values['n_time_points']:
-                missing_tracks.append(track_id)
-            else:
-                cleaned_rows.append(track_df.iloc[:self.values['n_time_points']])
-                #cleaned_rows.append(pd.DataFrame([[""] * len(df.columns)], columns=df.columns))  # Пустая строка
-
-        result_df = pd.concat(cleaned_rows, ignore_index=True)
-
-        # Проверка количества треков
-        if len(df["Track n"].unique()) != self.values['n_tracks']:
-            QMessageBox.warning(self, "Предупреждение", f"Ожидалось {self.values['n_tracks']} треков, но найдено {len(df['Track n'].unique())}.")
-
-        if missing_tracks:
-            QMessageBox.warning(self, "Недостаточно слайсов", f"У следующих треков меньше {self.values['n_time_points']} слайсов: {missing_tracks}")
-
-        return result_df
-    
-    def norm(self, data):
+    def norm(self):
         calculate_dxdy = False
 
-        if 'ΔX' not in data.columns or 'ΔY' not in data.columns:
-            data['ΔX'] = np.nan
-            data['ΔY'] = np.nan
+        if 'ΔX' not in self.data.columns or 'ΔY' not in self.data.columns:
+            self.data['ΔX'] = np.nan
+            self.data['ΔY'] = np.nan
             calculate_dxdy = True
 
-        data["magnitude"] = np.nan
-        data["cos_theta"] = np.nan
-        data["sin_theta"] = np.nan
+        self.data["magnitude"] = np.nan
+        self.data["cos_theta"] = np.nan
+        self.data["sin_theta"] = np.nan
 
-        for track_id in sorted(data["Track n"].unique()):
-            track_df = data[data["Track n"] == track_id].sort_values("Slice n")
+        for track_id in sorted(self.data["Track n"].unique()):
+            track_df = self.data[self.data["Track n"] == track_id].sort_values("Slice n")
             if calculate_dxdy:
                 track_df['ΔX'] = track_df['X'].diff()
                 track_df['ΔY'] = track_df['Y'].diff()
-            data.loc[track_df.index, 'ΔX'] = track_df["ΔX"].values
-            data.loc[track_df.index, 'ΔY'] = track_df["ΔY"].values
+            self.data.loc[track_df.index, 'ΔX'] = track_df["ΔX"].values
+            self.data.loc[track_df.index, 'ΔY'] = track_df["ΔY"].values
             
             track_df['magnitude'] = np.sqrt(track_df['ΔX']**2 + track_df['ΔY']**2)
-            data.loc[track_df.index, 'magnitude'] = track_df['magnitude'].values
+            self.data.loc[track_df.index, 'magnitude'] = track_df['magnitude'].values
 
             # Косинус и синус угла относительно оси X
             track_df['cos_theta'] = track_df['ΔX'] / track_df['magnitude']
             track_df['sin_theta'] = track_df['ΔY'] / track_df['magnitude']
-            data.loc[track_df.index, 'cos_theta'] = track_df['cos_theta'].values
-            data.loc[track_df.index, 'sin_theta'] = track_df['sin_theta'].values
-
-        return data
+            self.data.loc[track_df.index, 'cos_theta'] = track_df['cos_theta'].values
+            self.data.loc[track_df.index, 'sin_theta'] = track_df['sin_theta'].values
     
-    def scalars(self, data):
+    def scalars(self):
         for step in range(1, self.values['n_plot_points'] + 1):
             time_label = f"time_{step * self.values['time_interval']}"
             col_name = f"scalar_{time_label}"
             
             # Инициализируем колонку
-            data[col_name] = np.nan
+            self.data[col_name] = np.nan
             avg_scalars = []
             avg_sem = []
-            for track_id in sorted(data["Track n"].unique()):
-                track_df = data[data["Track n"] == track_id].sort_values("Slice n").reset_index()
+            for track_id in sorted(self.data["Track n"].unique()):
+                track_df = self.data[self.data["Track n"] == track_id].sort_values("Slice n").reset_index()
 
                 dx = track_df['cos_theta'].to_numpy()
                 dy = track_df['sin_theta'].to_numpy()
@@ -243,43 +84,39 @@ class UIAutocorrelation(QWidget):
                 err = np.std(avg_scalars, ddof=1) / np.sqrt(len(avg_scalars))
                 combined = f"{avg:.3f} ± {err:.3f}"
 
-                data.loc[0, col_name] = combined
+                self.data.loc[0, col_name] = avg
+                self.data.loc[1, col_name] = err
 
-        return data
-                    
-    def plot_scalar_averages(self, ax):
-        plt.figure(figsize=(10, 6))
-        for df, label in self.all_scalar_data:
-            time_points = [0]
-            scalar_values = [1]
 
-            for col in df.columns:
-                if col.startswith("scalar_time_"):
-                    match = re.search(r"scalar_time_(\d+)", col)
-                    if match:
-                        time_point = int(match.group(1))
-                        time_points.append(time_point)
+def plot_scalar_averages(ax, all_scalar_data):
+    ax.clear()
+    for df, label in all_scalar_data:
+        time_points = [0]
+        scalar_values = [1]
 
-                        vals = df[col].dropna().unique()
+        for col in df.columns:
+            if col.startswith("scalar_time_"):
+                match = re.search(r"scalar_time_(\d+)", col)
+                if match:
+                    time_point = int(match.group(1))
+                    time_points.append(time_point)
 
-                        scalars = []
-                        for val in vals:
-                            try:
-                                avg_str = val.split("±")[0].strip()
-                                scalars.append(float(avg_str))
-                            except:
-                                continue
+                    vals = df[col].dropna().unique()
+                    val = vals[0]
+                    scalar_values.append(float(val))
 
-                        scalar_values.append(np.mean(scalars) if scalars else np.nan)
+        # Сортировка по времени
+        sorted_pairs = sorted(zip(time_points, scalar_values))
+        sorted_times, sorted_scalars = zip(*sorted_pairs)
 
-            # Сортировка по времени
-            sorted_pairs = sorted(zip(time_points, scalar_values))
-            sorted_times, sorted_scalars = zip(*sorted_pairs)
-
+        try:
             ax.plot(sorted_times, sorted_scalars, marker='o', label=label)
+        except Exception as e:
+            print(label, e)
 
-        ax.set_xlabel("Time (step × interval)")
-        ax.set_ylabel("Average Scalar Product")
-        ax.set_title("Autocorrelation Scalar Averages")
-        ax.grid(True)
-        ax.legend()
+    ax.set_xlabel("Time (step × interval)")
+    ax.set_ylabel("Persistence")
+    ax.set_title("Migration Persistence")
+    ax.grid(True)
+    ax.legend()
+        

src/Analysis/dir_ratio.py
@@ -1,209 +1,48 @@
-from ui.configuration.configuration_autocorrelation_window import Ui_ConfigurationAutocorrelationWindow
-from ui.configuration.choose_sample_window import Ui_ChooseSampleWindow
-from ui.plot.plot_window import Ui_Plot
-from src.Plot.plot import PlotDialog
-from src.data_model import DataModel
-import pandas as pd
 import numpy as np
-import re
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
 
-from PyQt6.QtWidgets import (
-    QWidget, QDialog, QMessageBox, QTreeWidgetItem, QTabWidget, QVBoxLayout
-)
-
-class UIDirRatio(QWidget):
-    def __init__(self, ui):
+class DirRatio():
+    def __init__(self, data, values):
         super().__init__()
-        self.ui = ui
-        self.avg_dir_data = []
-        self.ui.actionDirectionality_Ratio.triggered.connect(self.open_dialog)
+        self.data = data
+        self.values = values
 
-    def open_dialog(self):
-        dialog = QDialog(self)
-        ui = Ui_ConfigurationAutocorrelationWindow()
-        ui.setupUi(dialog)
-        ui.lineEdit_n_plot_points.setDisabled(True)
-        
-        result = dialog.exec()
-        if result == QDialog.DialogCode.Accepted:
-            self.open_choose_samples_dialog()
-            self.values = self._read_values(ui)
-            if self.values is not None:
-                self.run_dir_ratio()
-        else:
-            return None
+        self.dir_ratio()
 
-    def _read_values(self, ui):
-        try:
-            time_interval = float(ui.lineEdit_time_interval.text())
-            n_time_points = int(ui.lineEdit_n_time_points.text())
-            n_tracks = int(ui.lineEdit_n_tracks.text())
-            n_plot_points = int(ui.lineEdit_n_plot_points.text())
-            return {
-                "time_interval": time_interval,
-                "n_time_points": n_time_points,
-                "n_tracks": n_tracks,
-                "n_plot_points": n_plot_points
-            }
-        except ValueError:
-            QMessageBox.warning(self, "Ошибка", "Пожалуйста, введите корректные числовые значения.")
-            return None
-
-    def open_choose_samples_dialog(self):
-        dialog = QDialog(self)
-        ui = Ui_ChooseSampleWindow()
-        ui.setupUi(dialog)
-        self.selected_samples = []
-
-        if self.ui.treeWidget:
-            ui.treeWidget.clear()
-            stack = []
-
-            # Клонируем верхний уровень
-            for i in range(self.ui.treeWidget.topLevelItemCount()):
-                src_item = self.ui.treeWidget.topLevelItem(i)
-                cloned_item = QTreeWidgetItem([src_item.text(0)])
-                ui.treeWidget.addTopLevelItem(cloned_item)
-                stack.append((src_item, cloned_item))
-
-            # Обход в ширину
-            while stack:
-                src_parent, cloned_parent = stack.pop(0)
-                for j in range(src_parent.childCount()):
-                    src_child = src_parent.child(j)
-                    cloned_child = QTreeWidgetItem([src_child.text(0)])
-                    cloned_parent.addChild(cloned_child)
-                    stack.append((src_child, cloned_child))
-            
-            
-            for i in range(ui.treeWidget.topLevelItemCount()):
-                ui.treeWidget.topLevelItem(i).setExpanded(True)
-
-            # Запускаем диалог и ждем результата
-            if dialog.exec() == QDialog.DialogCode.Accepted:
-                for item in ui.treeWidget.selectedItems():
-                    if item.parent() is not None:  # это лист (имеет родителя)
-                        file_path = item.parent().text(0)
-                        sheet_name = item.text(0)
-                        self.selected_samples.append([file_path, sheet_name])
-            else:
-                return None
-
-    def run_dir_ratio(self):
-        if self.values['time_interval'] == 0:
-            QMessageBox.warning(self, "Ошибка", "Пожалуйста, введите корректные числовые значения.")
-
-        for filename in self.selected_samples:
-            # Find sheet
-            index = None
-            for i in range(self.ui.stackedWidget.count()):
-                widget = self.ui.stackedWidget.widget(i)
-                if getattr(widget, 'filename', None) == filename[0]:
-                    index = i
-            
-            if index is not None:
-                    self.ui.stackedWidget.setCurrentIndex(index)
-            tab_widget = self.ui.stackedWidget.currentWidget().findChild(QTabWidget)
-            if tab_widget:
-                for i in range(tab_widget.count()):
-                    if tab_widget.tabText(i) == filename[1]:
-                        tab_widget.setCurrentIndex(i)
-                        break
-            
-            table = tab_widget.widget(i)
-            data_model = table.model()
-            if not hasattr(data_model, "get_dataframe"):
-                continue
-
-            original_df = data_model.get_dataframe()
-            data = self.treat_data(original_df)
-            if data is None:
-                continue
-
-            # Directionality Data
-            dir_data = self.dir_ratio(data)
-            self.avg_dir_data.append((dir_data, f"{filename[1]}"))
-
-            #Update table in tab:
-            new_model = DataModel(dir_data)
-            table.setModel(new_model)
-
-        # create plot
-        dialog = PlotDialog(self, title="MSD Plot")
-        dialog.show_plot(self.plot_dir_ratio)
-        #self.plot_dir_ratio()
-
-    def treat_data(self, df):
-        # Считать в pandas DataFrame
-        try:
-            df["Track n"] = df["Track n"].astype(int)
-            df["Slice n"] = df["Slice n"].astype(int)
-            df["X"] = pd.to_numeric(df["X"], errors='coerce')
-            df["Y"] = pd.to_numeric(df["Y"], errors='coerce')
-        except Exception as e:
-            QMessageBox.warning(self, "Ошибка", f"Не удалось преобразовать столбцы Track n или Slice n в числа:\n{e}")
-            return None
-
-        # Обработка треков
-        cleaned_rows = []
-        missing_tracks = []
-        for track_id in sorted(df["Track n"].unique()):
-            track_df = df[df["Track n"] == track_id].sort_values("Slice n")
-            if len(track_df) < self.values['n_time_points']:
-                missing_tracks.append(track_id)
-            else:
-                cleaned_rows.append(track_df.iloc[:self.values['n_time_points']])
-                #cleaned_rows.append(pd.DataFrame([[""] * len(df.columns)], columns=df.columns))  # Пустая строка
-
-        result_df = pd.concat(cleaned_rows, ignore_index=True)
-
-        # Проверка количества треков
-        if len(df["Track n"].unique()) != self.values['n_tracks']:
-            QMessageBox.warning(self, "Предупреждение", f"Ожидалось {self.values['n_tracks']} треков, но найдено {len(df['Track n'].unique())}.")
-
-        if missing_tracks:
-            QMessageBox.warning(self, "Недостаточно слайсов", f"У следующих треков меньше {self.values['n_time_points']} слайсов: {missing_tracks}")
-
-        return result_df
-    
-    def dir_ratio(self, data):
+    def dir_ratio(self):
         calculate_dxdy = False
         calculate_time = False
         calculate_dbp = False
         calculate_speed = False
 
-        if 'ΔX' not in data.columns or 'ΔY' not in data.columns:
-            data['ΔX'] = np.nan
-            data['ΔY'] = np.nan
+        if 'ΔX' not in self.data.columns or 'ΔY' not in self.data.columns:
+            self.data['ΔX'] = np.nan
+            self.data['ΔY'] = np.nan
             calculate_dxdy = True
         
-        if 'time' not in data.columns:
-            data['time'] = np.nan
+        if 'time' not in self.data.columns:
+            self.data['time'] = np.nan
             calculate_time = True
 
-        if 'distance_bw_points' not in data.columns:
-            data['distance_bw_points'] = np.nan
+        if 'distance_bw_points' not in self.data.columns:
+            self.data['distance_bw_points'] = np.nan
             calculate_dbp = True
 
-        if 'instant_speed' not in data.columns:
-            data["instant_speed"] = np.nan
+        if 'instant_speed' not in self.data.columns:
+            self.data["instant_speed"] = np.nan
             calculate_speed = True
 
-        data['Δ(xi-x0)'] = np.nan
-        data['Δ(yi-y0)'] = np.nan
-        data["distance_to_start"] = np.nan
-        data["cumulative_distance"] = np.nan
-        data["dir_ratio"] = np.nan
-        data["average_dir_ratio"] = np.nan
-        data["sem_average_dir_ratio"] = np.nan
+        self.data['Δ(xi-x0)'] = np.nan
+        self.data['Δ(yi-y0)'] = np.nan
+        self.data["distance_to_start"] = np.nan
+        self.data["cumulative_distance"] = np.nan
+        self.data["dir_ratio"] = np.nan
+        self.data["average_dir_ratio"] = np.nan
+        self.data["sem_average_dir_ratio"] = np.nan
 
         avr_dir = []
 
-        for track_id in sorted(data["Track n"].unique()):
-            track_df = data[data["Track n"] == track_id].sort_values("Slice n")
+        for track_id in sorted(self.data["Track n"].unique()):
+            track_df = self.data[self.data["Track n"] == track_id].sort_values("Slice n")
 
             if calculate_dxdy:
                 track_df['ΔX'] = track_df['X'].diff()
@@ -232,7 +71,7 @@ class UIDirRatio(QWidget):
             track_df["dir_ratio"] = track_df["dir_ratio"].replace([np.inf, -np.inf], np.nan)
 
             # Вставляем обратно все рассчитанные колонки
-            data.loc[track_df.index, ['ΔX', 'ΔY', 'Δ(xi-x0)', 'Δ(yi-y0)', 'time', 'instant_speed', 'distance_bw_points', 'distance_to_start', 'cumulative_distance', "dir_ratio"]] = \
+            self.data.loc[track_df.index, ['ΔX', 'ΔY', 'Δ(xi-x0)', 'Δ(yi-y0)', 'time', 'instant_speed', 'distance_bw_points', 'distance_to_start', 'cumulative_distance', "dir_ratio"]] = \
                 track_df[['ΔX', 'ΔY', 'Δ(xi-x0)', 'Δ(yi-y0)', 'time', 'instant_speed', 'distance_bw_points', 'distance_to_start', 'cumulative_distance', "dir_ratio"]]
 
             # Вставляем среднюю скорость в первую строку трека
@@ -242,27 +81,27 @@ class UIDirRatio(QWidget):
             avr_dir = np.array(avr_dir)
             avg = np.mean(avr_dir, axis=0)
             err = np.std(avr_dir, axis=0, ddof=1) / np.sqrt(avr_dir.shape[0])
-            data.loc[:self.values['n_time_points']-1, "average_dir_ratio"] = avg
-            data.loc[:self.values['n_time_points']-1, "sem_average_dir_ratio"] = err
+            self.data.loc[:self.values['n_time_points']-1, "average_dir_ratio"] = avg
+            self.data.loc[:self.values['n_time_points']-1, "sem_average_dir_ratio"] = err
 
-        return data
-                    
-    def plot_dir_ratio(self, ax):
-        # Для каждого набора данных
-        for df, label in self.avg_dir_data:
-            if 'average_dir_ratio' not in df.columns or 'sem_average_dir_ratio' not in df.columns:
-                continue  # Пропускаем, если нет нужных колонок
 
-            # Предполагается, что есть n строк с msd_avg и msd_err
-            y = df['average_dir_ratio'].dropna().values
-            yerr = df['sem_average_dir_ratio'].dropna().values
-            x = np.arange(len(y)) * self.values['time_interval']  # временная шкала
 
-            ax.errorbar(x, y, yerr=yerr, fmt='o-', capsize=4, label=label)
+def plot_dir_ratio(ax, avg_dir_data, values):
+    # Для каждого набора данных
+    for df, label in avg_dir_data:
+        if 'average_dir_ratio' not in df.columns or 'sem_average_dir_ratio' not in df.columns:
+            continue  # Пропускаем, если нет нужных колонок
 
-        ax.set_xlabel("Time (step × interval)")
-        ax.set_ylabel("Directionality Ratio")
-        ax.set_title("Directionality Ratio with SEM")
-        ax.grid(True)
-        ax.legend()
+        # Предполагается, что есть n строк с msd_avg и msd_err
+        y = df['average_dir_ratio'].dropna().values
+        yerr = df['sem_average_dir_ratio'].dropna().values
+        x = np.arange(len(y)) * values['time_interval']  # временная шкала
+
+        ax.errorbar(x, y, yerr=yerr, fmt='o-', capsize=4, label=label)
+
+    ax.set_xlabel("Time (step × interval)")
+    ax.set_ylabel("Directionality Ratio")
+    ax.set_title("Directionality Ratio with SEM")
+    ax.grid(True)
+    ax.legend()
 

src/Analysis/msd.py
@@ -1,193 +1,31 @@
-from ui.configuration.configuration_autocorrelation_window import Ui_ConfigurationAutocorrelationWindow
-from ui.configuration.choose_sample_window import Ui_ChooseSampleWindow
-from ui.plot.plot_window import Ui_Plot
-from src.Plot.plot import PlotDialog
-from src.data_model import DataModel
-import pandas as pd
 import numpy as np
-import re
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
 
-from PyQt6.QtWidgets import (
-    QWidget, QDialog, QMessageBox, QTreeWidgetItem, QTabWidget, QVBoxLayout
-)
-
-class UIMSD(QWidget):
-    def __init__(self, ui):
+class MSD():
+    def __init__(self, data, values):
         super().__init__()
-        self.ui = ui
-        self.avg_msd_data = []
-        self.ui.actionMSD.triggered.connect(self.open_dialog)
+        self.data = data
+        self.values = values
 
-    def open_dialog(self):
-        dialog = QDialog(self)
-        ui = Ui_ConfigurationAutocorrelationWindow()
-        ui.setupUi(dialog)
-        ui.lineEdit_n_plot_points.setDisabled(True)
-        
-        result = dialog.exec()
-        if result == QDialog.DialogCode.Accepted:
-            self.open_choose_samples_dialog()
-            self.values = self._read_values(ui)
-            if self.values is not None:
-                self.run_msd()
-        else:
-            return None
-
-    def _read_values(self, ui):
-        try:
-            time_interval = float(ui.lineEdit_time_interval.text())
-            n_time_points = int(ui.lineEdit_n_time_points.text())
-            n_tracks = int(ui.lineEdit_n_tracks.text())
-            n_plot_points = int(ui.lineEdit_n_plot_points.text())
-            return {
-                "time_interval": time_interval,
-                "n_time_points": n_time_points,
-                "n_tracks": n_tracks,
-                "n_plot_points": n_plot_points
-            }
-        except ValueError:
-            QMessageBox.warning(self, "Ошибка", "Пожалуйста, введите корректные числовые значения.")
-            return None
-
-    def open_choose_samples_dialog(self):
-        dialog = QDialog(self)
-        ui = Ui_ChooseSampleWindow()
-        ui.setupUi(dialog)
-        self.selected_samples = []
-
-        if self.ui.treeWidget:
-            ui.treeWidget.clear()
-            stack = []
-
-            # Клонируем верхний уровень
-            for i in range(self.ui.treeWidget.topLevelItemCount()):
-                src_item = self.ui.treeWidget.topLevelItem(i)
-                cloned_item = QTreeWidgetItem([src_item.text(0)])
-                ui.treeWidget.addTopLevelItem(cloned_item)
-                stack.append((src_item, cloned_item))
-
-            # Обход в ширину
-            while stack:
-                src_parent, cloned_parent = stack.pop(0)
-                for j in range(src_parent.childCount()):
-                    src_child = src_parent.child(j)
-                    cloned_child = QTreeWidgetItem([src_child.text(0)])
-                    cloned_parent.addChild(cloned_child)
-                    stack.append((src_child, cloned_child))
-            
-            
-            for i in range(ui.treeWidget.topLevelItemCount()):
-                ui.treeWidget.topLevelItem(i).setExpanded(True)
-
-            # Запускаем диалог и ждем результата
-            if dialog.exec() == QDialog.DialogCode.Accepted:
-                for item in ui.treeWidget.selectedItems():
-                    if item.parent() is not None:  # это лист (имеет родителя)
-                        file_path = item.parent().text(0)
-                        sheet_name = item.text(0)
-                        self.selected_samples.append([file_path, sheet_name])
-            else:
-                return None
-
-    def run_msd(self):
-        if self.values['time_interval'] == 0:
-            QMessageBox.warning(self, "Ошибка", "Пожалуйста, введите корректные числовые значения.")
-
-        for filename in self.selected_samples:
-            # Find sheet
-            index = None
-            for i in range(self.ui.stackedWidget.count()):
-                widget = self.ui.stackedWidget.widget(i)
-                if getattr(widget, 'filename', None) == filename[0]:
-                    index = i
-            
-            if index is not None:
-                    self.ui.stackedWidget.setCurrentIndex(index)
-            tab_widget = self.ui.stackedWidget.currentWidget().findChild(QTabWidget)
-            if tab_widget:
-                for i in range(tab_widget.count()):
-                    if tab_widget.tabText(i) == filename[1]:
-                        tab_widget.setCurrentIndex(i)
-                        break
-            
-            table = tab_widget.widget(i)
-            data_model = table.model()
-            if not hasattr(data_model, "get_dataframe"):
-                continue
-
-            original_df = data_model.get_dataframe()
-            data = self.treat_data(original_df)
-            if data is None:
-                continue
-
-            # MSD
-            msd_data = self.msd(data)
-            self.avg_msd_data.append((msd_data, f"{filename[1]}"))
-
-            #Update table in tab:
-            # Обновляем модель
-            new_model = DataModel(msd_data)
-            table.setModel(new_model)
-
-        # create plot
-        dialog = PlotDialog(self, title="MSD Plot")
-        dialog.show_plot(self.plot_msd)
-        #self.plot_msd()
-
-    def treat_data(self, df):
-        # Считать в pandas DataFrame
-        try:
-            df["Track n"] = df["Track n"].astype(int)
-            df["Slice n"] = df["Slice n"].astype(int)
-            df["X"] = pd.to_numeric(df["X"], errors='coerce')
-            df["Y"] = pd.to_numeric(df["Y"], errors='coerce')
-        except Exception as e:
-            QMessageBox.warning(self, "Ошибка", f"Не удалось преобразовать столбцы Track n или Slice n в числа:\n{e}")
-            return None
-
-        # Обработка треков
-        cleaned_rows = []
-        missing_tracks = []
-        for track_id in sorted(df["Track n"].unique()):
-            track_df = df[df["Track n"] == track_id].sort_values("Slice n")
-            if len(track_df) < self.values['n_time_points']:
-                missing_tracks.append(track_id)
-            else:
-                cleaned_rows.append(track_df.iloc[:self.values['n_time_points']])
-                #cleaned_rows.append(pd.DataFrame([[""] * len(df.columns)], columns=df.columns))  # Пустая строка
-
-        result_df = pd.concat(cleaned_rows, ignore_index=True)
-
-        # Проверка количества треков
-        if len(df["Track n"].unique()) != self.values['n_tracks']:
-            QMessageBox.warning(self, "Предупреждение", f"Ожидалось {self.values['n_tracks']} треков, но найдено {len(df['Track n'].unique())}.")
-
-        if missing_tracks:
-            QMessageBox.warning(self, "Недостаточно слайсов", f"У следующих треков меньше {self.values['n_time_points']} слайсов: {missing_tracks}")
-
-        return result_df
+        self.msd()
     
-    def msd(self, data):
+    def msd(self):
         calculate_time = False
-        if 'time' not in data.columns:
-            data['time'] = np.nan
+        if 'time' not in self.data.columns:
+            self.data['time'] = np.nan
             calculate_time = True
         
-        data['avg_msd_by_time_cell'] = np.nan
-        data["avg_msd_by_time_condition"] = np.nan
-        data["sem_by_time_condition"] = np.nan
+        self.data['avg_msd_by_time_cell'] = np.nan
+        self.data["avg_msd_by_time_condition"] = np.nan
+        self.data["sem_by_time_condition"] = np.nan
 
         avr_msd = []
 
-        for track_id in sorted(data["Track n"].unique()):
-            track_df = data[data["Track n"] == track_id].sort_values("Slice n")
+        for track_id in sorted(self.data["Track n"].unique()):
+            track_df = self.data[self.data["Track n"] == track_id].sort_values("Slice n")
 
             if calculate_time:
                 track_df['time'] = track_df['Slice n'] * self.values['time_interval']
-                data.loc[track_df.index, 'time'] = track_df["time"].values
+                self.data.loc[track_df.index, 'time'] = track_df["time"].values
 
             # Calculate MSD
             msd = np.zeros(self.values['n_time_points'])
@@ -200,7 +38,7 @@ class UIMSD(QWidget):
                 msd[tau] = np.mean(displacements)
 
             track_df['avg_msd_by_time_cell'] = msd
-            data.loc[track_df.index, 'avg_msd_by_time_cell'] = track_df["avg_msd_by_time_cell"].values
+            self.data.loc[track_df.index, 'avg_msd_by_time_cell'] = track_df["avg_msd_by_time_cell"].values
             
             avr_msd.append(msd)
 
@@ -208,26 +46,24 @@ class UIMSD(QWidget):
             avr_msd = np.array(avr_msd)
             avg = np.mean(avr_msd, axis=0)
             err = np.std(avr_msd, axis=0, ddof=1) / np.sqrt(avr_msd.shape[0])
-            data.loc[:self.values['n_time_points']-1, "avg_msd_by_time_condition"] = avg
-            data.loc[:self.values['n_time_points']-1, "sem_by_time_condition"] = err
+            self.data.loc[:self.values['n_time_points']-1, "avg_msd_by_time_condition"] = avg
+            self.data.loc[:self.values['n_time_points']-1, "sem_by_time_condition"] = err
 
-        return data
-                    
-    def plot_msd(self, ax):
-        # Для каждого набора данных
-        for df, label in self.avg_msd_data:
-            if 'avg_msd_by_time_condition' not in df.columns or 'sem_by_time_condition' not in df.columns:
-                continue  # Пропускаем, если нет нужных колонок
+def plot_msd(ax, avg_msd_data, values):
+    # Для каждого набора данных
+    for df, label in avg_msd_data:
+        if 'avg_msd_by_time_condition' not in df.columns or 'sem_by_time_condition' not in df.columns:
+            continue  # Пропускаем, если нет нужных колонок
 
-            # Предполагается, что есть n строк с msd_avg и msd_err
-            y = df['avg_msd_by_time_condition'].dropna().values
-            yerr = df['sem_by_time_condition'].dropna().values
-            x = np.arange(len(y)) * self.values['time_interval']  # временная шкала
+        # Предполагается, что есть n строк с msd_avg и msd_err
+        y = df['avg_msd_by_time_condition'].dropna().values
+        yerr = df['sem_by_time_condition'].dropna().values
+        x = np.arange(len(y)) * values['time_interval']  # временная шкала
 
-            ax.errorbar(x, y, yerr=yerr, fmt='o-', capsize=4, label=label)
+        ax.errorbar(x, y, yerr=yerr, fmt='o-', capsize=4, label=label)
 
-        ax.set_xlabel("Time (step × interval)")
-        ax.set_ylabel("Mean Squared Displacement (MSD)")
-        ax.set_title("MSD with SEM")
-        ax.grid(True)
-        ax.legend()
+    ax.set_xlabel("Time (step × interval)")
+    ax.set_ylabel("Mean Squared Displacement (MSD)")
+    ax.set_title("MSD with SEM")
+    ax.grid(True)
+    ax.legend()